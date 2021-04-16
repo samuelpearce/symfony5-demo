0a01285be5d7de1278a55aa3993b4cf3ae77a152
Samuel Pearce <samuelcpearce@gmail.com>
2021-04-16 01:53:51 +0100

    Add Country to Person field
    php bin/console make:entity Person
    
    The command lets you append values but not remove
    
    Don't forget migrations
    bin/console make:migration
    bin/console doctrine:migrations:migrate

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -34,6 +34,11 @@ class Person
      */
     private $occupation;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -71,6 +76,18 @@ class Person
     public function setOccupation(?Occupation $occupation): self
     {
         $this->occupation = $occupation;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
 
         return $this;
     }
