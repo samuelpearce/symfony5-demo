c675ee25872c17459f9bc8fe10aa51ba6f2024e8
Samuel Pearce <samuelcpearce@gmail.com>
2021-04-16 01:39:56 +0100

    Make Migration of current entities in DB
    php bin/console make:migration
    
    php bin/console doctrine:migrations:migrate
    
    If you get errors, follow the on-screen instructions. If you get stuck
    
    php bin/console cache:clear
    
    If this is still a problem, check your .env.local serverVersion matches your MySQL server version

---
+++ migrations/Version20210416003331.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210416003331 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE occupation (id INT AUTO_INCREMENT NOT NULL, occupation VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE person (id INT AUTO_INCREMENT NOT NULL, occupation_id INT DEFAULT NULL, name VARCHAR(50) NOT NULL, email VARCHAR(100) NOT NULL, INDEX IDX_34DCD17622C8FC20 (occupation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE person ADD CONSTRAINT FK_34DCD17622C8FC20 FOREIGN KEY (occupation_id) REFERENCES occupation (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE person DROP FOREIGN KEY FK_34DCD17622C8FC20');
+        $this->addSql('DROP TABLE occupation');
+        $this->addSql('DROP TABLE person');
+    }
+}
